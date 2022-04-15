79cf0d2b6fe64bf682a4a18d35347c5491d18871
RodrigoSCarvalho <carvalho_rodrigo@id.uff.br>
2022-04-15 00:58:50 -0300

    Initial commit

--- TDD - Fatura/test/test/FaturaTest.java
+++ TDD - Fatura/test/test/FaturaTest.java
@@ -1,35 +1,6 @@
 package test;
 
-import static org.junit.Assert.assertEquals;
-
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.jupiter.api.BeforeEach;
-
-import junit.framework.Assert;
-import src.Boleto;
-import src.Fatura;
-
 public class FaturaTest {
-	
-	Fatura fatura;
-	Boleto boleto1;
-	Boleto boleto2;
-	Boleto boleto3;
-	
-	
-	@Before
-	public void inicializa() {
-		this.fatura = new Fatura();
-		this.boleto1 = new Boleto();
-		this.boleto2 = new Boleto();
-		this.boleto3 = new Boleto();
-	}
-	
-	@Test
-	public void testFaturaPaga() {
-		assertEquals("PAGA", processadorDeBoletos());
-	}
 	
 
 }
